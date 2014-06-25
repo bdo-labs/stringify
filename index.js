376c9f4aaee915e6dd4d254ed3f0cff7ac44e641
Henrik Kjelsberg <hkjels@me.com>
2014-06-25 09:34:11 +0200

    Replaced the `stringify`-function

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-var through = require('through');
 
 /**
- * Stringifies the content
- * @param   {string}    content
- * @returns {string}
+ * Module dependencies.
  */
-function stringify(content) {
-	var stringified_content;
-
-	stringified_content = content.replace(/\"/g, '\u005C\u0022');
-	stringified_content = stringified_content.replace(/^(.*)/gm, '"$1');
-	stringified_content = stringified_content.replace(/(.+)$/gm, '$1" +');
-	stringified_content = stringified_content.replace(/\+$/, '');
-
-	return 'module.exports = ' + stringified_content + ';\n';
-}
+var through = require('through');
+var stringify = require('string-to-js');
 
 /**
  * Creates the
@@ -24,64 +13,64 @@ function stringify(content) {
  */
 module.exports = function(options) {
 
-	/**
-	 * The file extensions of file which should be stringified
-	 * @type    {string[]}
-	 */
-	var extensions = [
-		'.text',
-		'.txt',
-		'.html',
-		'.tmpl',
-		'.tpl'
-	];
+  /**
+   * The file extensions of file which should be stringified
+   * @type    {string[]}
+   */
+  var extensions = [
+    '.text',
+    '.txt',
+    '.html',
+    '.tmpl',
+    '.tpl'
+  ];
 
-	if (options) {
-		if (Object.prototype.toString.call(options) === '[object Array]') {
-			extensions = options;
-		} else if(options.extensions) {
-			extensions = options.extensions;
-		}
-	}
+  if (options) {
+    if (Object.prototype.toString.call(options) === '[object Array]') {
+      extensions = options;
+    } else if(options.extensions) {
+      extensions = options.extensions;
+    }
+  }
 
-	/**
-	 * Returns whether the file ends in an extension
-	 * @param   {string} file
-	 * @return  {boolean}
-	 */
-	function has_stringify_extension(file) {
-		for (var i=0; i<extensions.length; ++i) {
-			if (file.substr(-1*extensions[i].length) === extensions[i]) {
-				return true;
-			}
-		}
-		return false;
-	}
+  /**
+   * Returns whether the file ends in an extension
+   * @param   {string} file
+   * @return  {boolean}
+   */
+  function has_stringify_extension(file) {
+    for (var i=0; i<extensions.length; ++i) {
+      if (file.substr(-1*extensions[i].length) === extensions[i]) {
+        return true;
+      }
+    }
+    return false;
+  }
 
-	/**
-	 * The browserify transform method
-	 * @param   {string} file
-	 * @returns {stream}
-	 */
-	function transform(file) {
-		var content = '';
+  /**
+   * The browserify transform method
+   * @param   {string} file
+   * @returns {stream}
+   */
+  function transform(file) {
+    var content = '';
 
-		if (!has_stringify_extension(file)) {
-			return through();
-		}
+    if (!has_stringify_extension(file)) {
+      return through();
+    }
 
+    function write(buffer) {
+      content += buffer;
+    }
 
-		function write(buffer) {
-			content += buffer;
-		}
+    function end() {
+      this.queue(stringify(content));
+      this.queue(null);
+    }
 
-		function end() {
-			this.queue(stringify(content));
-			this.queue(null);
-		}
+    return through(write, end);
+  };
 
-		return through(write, end);
-	};
-
-	return transform;
+  return transform;
 };
+
